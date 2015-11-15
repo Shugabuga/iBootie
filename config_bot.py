89a9494e7a56c3ad869dcc17841421dfa253abcd
Shugabuga <bp1004blogs@gmail.com>
2015-11-14 19:10:32 -0500

    Made this more useful.

--- config_bot.py
+++ config_bot.py
@@ -1,3 +1,17 @@
-#Bot login information goes here!
-REDDIT_USERNAME = 'BOT_USERNAME' #Username
-REDDIT_PASS = 'BOT_PASSWORD' #Password
+#Configuration for Bot
+REDDIT_USERNAME = 'BOT USERNAME' #Username
+REDDIT_PASS = 'BOT PASSWORD' #Password
+SUBREDDIT = 'all' #Enter subreddit to scan, or remain to scan /r/all.
+user_agent = 'NAME_OF_BOT Version 1' #Please enter your bot name and its version.
+KEYWORD = 'SEARCH TERM' #Term to look in post.
+REPLY = '''
+Enter message here.
+
+**This is bold**
+
+*This is italic.*
+
+[Text Displayed by Link](https://link.com)
+
+These lines have to be double-spaced to be registered as seperate lines.
+''' #Reply message.
